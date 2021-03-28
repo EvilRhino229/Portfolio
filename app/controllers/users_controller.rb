33309f6b6af7a1997934c6ee42df00d8c32030e1
Timothy Kepple <timkep1@yahoo.com>
2021-03-28 01:59:32 -0500

    Admin user added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!, only: [:admin_dash]
+  before_action :authenticate_user!, only: [:admin_dash]
     def admin_dash
     if user_signed_in? && current_user.admin?
     else
